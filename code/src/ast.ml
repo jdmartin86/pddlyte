f5517abe43f207741f61798f07c637b6adefd0c3
John Martin <jdmartin86@gmail.com>
2014-04-02 00:16:16 -0400

    supports define expressions

--- code/src/ast.ml
+++ code/src/ast.ml
@@ -10,23 +10,54 @@ type sym = string
 
 (* abstract syntax tree nodes *)
 type expr =
-  | Expr_unit                          (* () *) 
-  | Expr_int    of int                 (* 0-9 *)
-  | Expr_sym    of sym                 (* a-z *)
+  | Expr_unit                           (*  () *) 
+  | Expr_int    of int                  (* 0-9 *)
+  | Expr_sym    of sym                  (* a-z *)
   | Expr_lst    of expr list
-  | Expr_apply  of expr * expr list      
-  | Expr_define of sym * expr          (* ( define (1) ( 2 3 4 ) 5 ) *)
+  | Expr_define of sym * expr           (* ( define (1) ( 2 3 4 ) 5 ) *)
+  | Expr_pred   of sym * expr list
+(*
+  | Expr_action of sym * expr list
   | Expr_lambda of sym list * expr list (*  *)
-
+*)
+let ast_of_atom atm = 
+  ( match atm with
+      | Atom_unit  -> Expr_unit
+      | Atom_int n -> Expr_int n
+      | Atom_sym s -> Expr_sym s
+  )
 
 let rec ast_of_sexpr sx = match sx with
   | Expr_atom a ->
-    (match a with
+    ( match a with
       | Atom_unit  -> Expr_unit
       | Atom_int n -> Expr_int n
       | Atom_sym s -> Expr_sym s
     )
-  | Expr_list l ->  Expr_lst( List.map ast_of_sexpr l )
+  | Expr_list l ->
+    ( match l with (* distinguish atom lists and expr lists *)
+      | h::t -> 
+	( match h with 
+	  | Expr_atom a ->
+	    ( match a with
+	      | Atom_sym s -> 
+		( match s with
+		  | "define" -> 
+		    Expr_define( s , ast_of_sexpr (Expr_list t) )
+		  | ":predicates" -> 
+		    Expr_pred( s , List.map ast_of_sexpr t )
+		  | _ -> Expr_lst( List.map ast_of_sexpr l )
+		)
+	      | _ -> failwith "not a symbol -- unreachable"
+	    )
+	  | Expr_list ll -> ast_of_sexpr (Expr_list ll)
+	  | _ -> failwith "list head is not an atom -- unreachable"
+	)
+      | _ -> failwith "non-symbol head -- won't be a list"  
+    )
+  | _ -> failwith "singleton or empty list"
+
+     (* | Expr_lst( List.map ast_of_sexpr l ) *)
 (*   
      ( match l with
      |	[] -> ast_of_sexpr (Expr_atom (Atom_unit))
@@ -42,17 +73,15 @@ let rec ast_of_sexpr sx = match sx with
 	| h::t -> map f t (f h :: a)
     in map (ast_of_sexpr) l [])
 *)
-(*
- Expr_lst( List.map ( fun s -> ast_of_sexpr sx s ) l )
-*)
+
 let string_of_ast ast =
    let sprintf  = Printf.sprintf in  (* to make the code cleaner *)
    let spaces n = String.make n ' ' in
-   let rec string_of_ids id_lst = 
-      match id_lst with
+   let rec string_of_syms sym_lst = 
+      match sym_lst with
          | []   -> ""
-         | [id] -> id
-         | h::t -> h ^ " " ^ (string_of_ids t)
+         | [s] -> s
+         | h::t -> h ^ " " ^ (string_of_syms t)
    in
 
    let rec iter ast indent =
@@ -73,16 +102,9 @@ let string_of_ast ast =
 	 | Expr_define (id, e) -> 
               sprintf "%sDEFINE[%s\n%s ]" 
                  (spaces indent) id (iter e (indent + 2))
-         | Expr_lambda (ids, body) ->
-              sprintf "%sLAMBDA[(%s)%s ]"
-                 (spaces indent)
-                 (string_of_ids ids)
-                 (string_of_exprs body)
-         | Expr_apply (operator, operands) ->
-              sprintf "%sAPPLY[\n%s%s ]"
-                 (spaces indent)
-                 (iter operator (indent + 2))
-                 (string_of_exprs operands)
+         | Expr_pred (key, preds) ->
+              sprintf "%sPREDICATE[%s%s ]"
+                 (spaces indent) key (string_of_exprs preds)
    in
       "\n" ^ iter ast 0 ^ "\n"
 
--- code/src/ast.mli
+++ code/src/ast.mli
@@ -9,9 +9,8 @@ type expr =
   | Expr_int    of int                 (* 0-9 *)
   | Expr_sym    of sym                 (* a-z *)
   | Expr_lst  of expr list           (* ( ( 1 ) ( 2 ( 3 ) ) 4 ) *)
-  | Expr_apply of expr * expr list
   | Expr_define of sym * expr          (* ( define (1) ( 2 3 4 ) 5 ) *)
-  | Expr_lambda of sym list * expr list (*  *)
+  | Expr_pred   of sym * expr list
 
 (* convert s-expression into ast expression 
 val ast_of_sexpr : Sexpr.expr -> expr
