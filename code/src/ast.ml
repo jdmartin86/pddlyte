94b6a45c7412baa75a45d1cb0543e353e2c1bb60
John Martin <jdmartin86@gmail.com>
2014-04-03 23:42:28 -0400

    Added planner nodes and procedures

--- code/src/ast.ml
+++ code/src/ast.ml
@@ -6,20 +6,25 @@
 
 open Sexpr
 
+type proc = And | Or | Not
+
 type sym = string
 
 (* abstract syntax tree nodes *)
 type expr =
-  | Expr_unit                           (*  () *) 
-  | Expr_int    of int                  (* 0-9 *)
-  | Expr_sym    of sym                  (* a-z *)
+  | Expr_unit                            
+  | Expr_int    of int                  
+  | Expr_sym    of sym                  
   | Expr_lst    of expr list
-  | Expr_define of sym * expr           (* ( define (1) ( 2 3 4 ) 5 ) *)
-  | Expr_pred   of sym * expr list
-(*
-  | Expr_action of sym * expr list
-  | Expr_lambda of sym list * expr list (*  *)
-*)
+  | Expr_define of sym  * expr
+  | Expr_domain of sym  * expr list          
+  | Expr_pred   of sym  * expr list 
+  | Expr_action of sym  * expr list
+  | Expr_objs   of sym  * expr list
+  | Expr_init   of sym  * expr list
+  | Expr_goal   of sym  * expr list
+  | Expr_proc   of proc * expr list
+ 
 let ast_of_atom atm = 
   ( match atm with
       | Atom_unit  -> Expr_unit
@@ -28,34 +33,42 @@ let ast_of_atom atm =
   )
 
 let rec ast_of_sexpr sx = match sx with
-  | Expr_atom a ->
-    ( match a with
-      | Atom_unit  -> Expr_unit
-      | Atom_int n -> Expr_int n
-      | Atom_sym s -> Expr_sym s
-    )
+  | Expr_atom a -> ast_of_atom a
   | Expr_list l ->
     ( match l with (* distinguish atom lists and expr lists *)
+      | []   -> failwith "empty list not implemented"
+      | [x]  -> 
+	( match x with
+	  | Expr_list ll ->
+	    Expr_lst( List.map ast_of_sexpr ll )
+	  | _ -> failwith "no idea"
+	)
       | h::t -> 
 	( match h with 
-	  | Expr_atom a ->
-	    ( match a with
-	      | Atom_sym s -> 
-		( match s with
-		  | "define" -> 
-		    Expr_define( s , ast_of_sexpr (Expr_list t) )
-		  | ":predicates" -> 
-		    Expr_pred( s , List.map ast_of_sexpr t )
-		  | _ -> Expr_lst( List.map ast_of_sexpr l )
-		)
-	      | _ -> failwith "not a symbol -- unreachable"
-	    )
-	  | Expr_list ll -> ast_of_sexpr (Expr_list ll)
-	  | _ -> failwith "list head is not an atom -- unreachable"
+	  | Expr_atom( Atom_sym "define" ) -> 
+	    Expr_define( "define" , ast_of_sexpr (Expr_list t) )(* complete l *)
+	  | Expr_atom( Atom_sym ":predicates" ) -> 
+	    Expr_pred( ":predicates" , List.map ast_of_sexpr t )
+	  | Expr_atom( Atom_sym ":action" ) ->
+	    Expr_action( ":action", List.map ast_of_sexpr t )
+	  | Expr_atom( Atom_sym ":domain" ) ->
+	    Expr_domain( ":domain" , List.map ast_of_sexpr t )
+	  | Expr_atom( Atom_sym ":objects" ) ->
+	    Expr_objs( ":objects" , List.map ast_of_sexpr t )
+	  | Expr_atom( Atom_sym ":init" ) ->
+	    Expr_init( ":init" , List.map ast_of_sexpr t )
+	  | Expr_atom( Atom_sym ":goal" ) ->
+	    Expr_goal( ":goal" , List.map ast_of_sexpr t )
+	  | Expr_atom( Atom_sym "and" ) ->
+	    Expr_proc( And , List.map ast_of_sexpr t )
+	  | Expr_atom( Atom_sym "not" ) ->
+	    Expr_proc( Not , List.map ast_of_sexpr t )
+	  | Expr_atom( Atom_sym "or" ) ->
+	    Expr_proc( Or , List.map ast_of_sexpr t )
+	  | _ -> Expr_lst( List.map ast_of_sexpr l )
 	)
-      | _ -> failwith "non-symbol head -- won't be a list"  
+      | _ -> failwith "list of list"
     )
-  | _ -> failwith "singleton or empty list"
 
      (* | Expr_lst( List.map ast_of_sexpr l ) *)
 (*   
@@ -69,11 +82,10 @@ let rec ast_of_sexpr sx = match sx with
   | Expr_list l -> Expr_lst(
     let rec map f lst a =
       match lst with
-	| [] -> a
+	| [] -> a 
 	| h::t -> map f t (f h :: a)
     in map (ast_of_sexpr) l [])
 *)
-
 let string_of_ast ast =
    let sprintf  = Printf.sprintf in  (* to make the code cleaner *)
    let spaces n = String.make n ' ' in
@@ -83,43 +95,69 @@ let string_of_ast ast =
          | [s] -> s
          | h::t -> h ^ " " ^ (string_of_syms t)
    in
-
    let rec iter ast indent =
-      let string_of_exprs e_list =
-         (List.fold_left (^) ""
-             (List.map
-                 (fun e -> "\n" ^ iter e (indent + 2))
-                 e_list))
-      in
-      match ast with
-         | Expr_unit    -> sprintf "%sUNIT"       (spaces indent) 
-         | Expr_int  i  -> sprintf "%sINT[ %d ]"  (spaces indent) i
-         | Expr_sym  s  -> sprintf "%sSYM[ %s ]"  (spaces indent) s
-	 | Expr_lst  l  ->
-	      sprintf "%sLIST[%s ]"
-	         (spaces indent ) 
-		 (string_of_exprs l )
-	 | Expr_define (id, e) -> 
-              sprintf "%sDEFINE[%s\n%s ]" 
-                 (spaces indent) id (iter e (indent + 2))
-         | Expr_pred (key, preds) ->
-              sprintf "%sPREDICATE[%s%s ]"
-                 (spaces indent) key (string_of_exprs preds)
+     let string_of_exprs e_list =
+       (List.fold_left (^) ""
+	  (List.map
+             (fun e -> "\n" ^ iter e (indent + 2))
+             e_list))
+     in
+     match ast with
+       | Expr_unit    -> sprintf "%sUNIT"       (spaces indent) 
+       | Expr_int  i  -> sprintf "%sINT[ %d ]"  (spaces indent) i
+       | Expr_sym  s  -> sprintf "%sSYM[ %s ]"  (spaces indent) s
+       | Expr_lst  l  ->
+	 sprintf "%sLIST[%s ]"
+	   (spaces indent ) 
+	   (string_of_exprs l )
+       | Expr_define (id, e) -> 
+	 sprintf "%sDEFINE[%s\n%s ]" 
+           (spaces indent) id (iter e (indent + 2))
+       | Expr_pred (key, preds) ->
+	 sprintf "%sPREDICATE[%s%s ]"
+           (spaces indent) key (string_of_exprs preds)
+       | Expr_action (key, preds) ->
+	 sprintf "%sACTION[%s%s ]"
+	   (spaces indent) key (string_of_exprs preds)
+       | Expr_domain (key, preds) ->
+	 sprintf "%sDOMAIN[%s%s ]"
+	   (spaces indent) key (string_of_exprs preds)
+       | Expr_objs (key, preds) ->
+	 sprintf "%sOBJECTS[%s%s ]"
+	   (spaces indent) key (string_of_exprs preds)
+       | Expr_init (key, preds) ->
+	 sprintf "%sINIT[%s%s ]"
+	   (spaces indent) key (string_of_exprs preds)
+       | Expr_goal (key, preds) ->
+	 sprintf "%sGOAL[%s%s ]"
+	   (spaces indent) key (string_of_exprs preds)
+       | Expr_proc (key, preds) ->
+	 ( match key with
+	   | And ->
+	     sprintf "%sPROC[%s%s ]"
+	       (spaces indent) "and" (string_of_exprs preds)
+	   | Or ->
+	     sprintf "%sPROC[%s%s ]"
+	       (spaces indent) "or" (string_of_exprs preds)
+	   | Not ->
+	     sprintf "%sPROC[%s%s ]"
+	       (spaces indent) "not" (string_of_exprs preds)
+	 )
    in
-      "\n" ^ iter ast 0 ^ "\n"
+   "\n" ^ iter ast 0 ^ "\n"
 
 let ast_test infile =
-   let lexbuf = Lexing.from_channel infile in
-   let rec loop () =
-      let sexpr  = Parser.parse Lexer.token lexbuf in
-         match sexpr with
-            | None -> ()
-            | Some s ->
-                 let expr = ast_of_sexpr s in
-                    Printf.printf "%s\n" (string_of_ast expr); 
-                    flush stdout;
-                    loop ()
-   in
-      loop ()
+  let lexbuf = Lexing.from_channel infile in
+  let rec loop () =
+    let sexpr  = Parser.parse Lexer.token lexbuf in
+    match sexpr with
+      | None -> ()
+      | Some s ->
+        let expr = ast_of_sexpr s in
+        Printf.printf "%s\n" (string_of_ast expr); 
+        flush stdout;
+        loop ()
+  in
+  loop ()
 
 
--- code/src/ast.mli
+++ code/src/ast.mli
@@ -1,16 +1,24 @@
 (* the abstract syntax tree *)
 
+type proc = And | Or | Not
+
 (* atoms *)
 type sym = string
 
 (* abstract syntax tree nodes *)
 type expr =
-  | Expr_unit                          (* () *) 
-  | Expr_int    of int                 (* 0-9 *)
-  | Expr_sym    of sym                 (* a-z *)
-  | Expr_lst  of expr list           (* ( ( 1 ) ( 2 ( 3 ) ) 4 ) *)
-  | Expr_define of sym * expr          (* ( define (1) ( 2 3 4 ) 5 ) *)
-  | Expr_pred   of sym * expr list
+  | Expr_unit                            
+  | Expr_int    of int                  
+  | Expr_sym    of sym                  
+  | Expr_lst    of expr list
+  | Expr_define of sym  * expr
+  | Expr_domain of sym  * expr list          
+  | Expr_pred   of sym  * expr list 
+  | Expr_action of sym  * expr list
+  | Expr_objs   of sym  * expr list
+  | Expr_init   of sym  * expr list
+  | Expr_goal   of sym  * expr list
+  | Expr_proc   of proc * expr list
 
 (* convert s-expression into ast expression 
 val ast_of_sexpr : Sexpr.expr -> expr
