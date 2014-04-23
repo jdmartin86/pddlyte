dfee6bd0c5a9ee8f7b6e13c39c4dea486210914f
John Martin <jdmartin86@gmail.com>
2014-04-23 19:57:46 -0400

    supports strips problem

--- code/src/pddlyte.ml
+++ code/src/pddlyte.ml
@@ -3,8 +3,7 @@ open Strips
 let run_program infile =
   let lexbuf = Lexing.from_channel infile in
   let env = Strips.make None in
-  let empty_strips_prob = Strips.make_strips in
-  let rec loop prob =
+  let rec loop () =
     let sexpr  = Parser.parse Lexer.token lexbuf in
     match sexpr with
       | None -> 
@@ -12,12 +11,10 @@ let run_program infile =
 	() 
       | Some s ->
         let ast = Ast.ast_of_sexpr s in
-        (*Printf.printf "%s\n" (string_of_ast ast);*)
-	let partial_strips_prob = 
-	  Strips.strips_of_ast env prob ast in
+	let _ = Strips.strips_of_ast env ast in
         flush stdout;
-        loop partial_strips_prob
-  in loop empty_strips_prob 
+        loop ()
+  in loop () 
 
 let _ = 
    if Array.length Sys.argv <> 2 then
