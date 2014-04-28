aa131f563d4aad8efccc21a57a79df11575bf3fb
John Martin <jdmartin86@gmail.com>
2014-04-28 07:23:49 -0400

    applicative actions and successor function partially tested

--- code/src/planner.ml
+++ code/src/planner.ml
@@ -2,6 +2,8 @@
 open Ast
 open Strips
 
+exception Planner_error of string 
+
 (* ( atom , atom ) Hashtbl *)
 module Atomhash = Hashtbl.Make
   (struct
@@ -9,6 +11,67 @@ module Atomhash = Hashtbl.Make
     let equal x y = x = y
     let hash = Hashtbl.hash
    end)
+
+
+(** test functions **)
+let sprintf  = Printf.sprintf   (* to make the code cleaner *)
+let spaces n = String.make n ' '
+
+let rec string_of_syms sym_lst = 
+  (match sym_lst with
+    | []   -> ""
+    | [s] -> s
+    | h::t -> h ^ " " ^ (string_of_syms t)
+  )
+
+let string_of_atom atom =
+  ( match atom with 
+    | Atom_var a -> a
+    | Atom_gnd a -> a
+    | Atom_nil -> ""
+  )
+
+let string_of_pred pred = 
+  ( match pred with 
+    | Pred_var( name , params ) ->
+      sprintf "PRED_VAR( %s , %s )\n" 
+	name 
+	(string_of_syms(List.map string_of_atom params))
+    | Pred_gnd( name , params ) ->
+      sprintf "PRED_GND( %s , %s )\n" 
+	name 
+	(string_of_syms(List.map string_of_atom params))
+    | Pred_nil -> 
+      "PRED_NIL()\n"
+  )
+
+let string_of_params params = 
+  sprintf "PARAMETERS( %s )"
+  (string_of_syms (List.map string_of_atom params))
+
+let rec string_of_conj conj = 
+  let recurse = string_of_conj in
+  ( match conj with 
+    | Conj_and c -> 
+      sprintf "CONJ_AND(%s)"
+      (string_of_syms (List.map recurse c))
+    | Conj_pos c -> 
+      sprintf "CONJ_POS(%s)"
+	(string_of_pred c)
+    | Conj_neg c -> 
+      sprintf "CONJ_NEG(%s)"
+	(string_of_pred c)
+    | Conj_nil -> ""
+  )
+
+let string_of_precond precond =
+  sprintf "PRECONDITION( %s )"
+  (string_of_conj precond)
+
+let string_of_effect effect =
+  sprintf "EFFECT( %s )"
+  (string_of_conj effect)
+(** end test functions **)
 
 (* ast.conjunction -> ast.predicate *)
 let pred_of_conj c =
@@ -43,20 +106,6 @@ let  ppreds_of_act act =
     | _ -> []
   )
 
-(* preconds -> pred *)
-let partition_conj_to_pred p = 
-  ( match p with 
-    | Conj_and c -> List.partition 
-      ( fun x -> match x with | Conj_pos _ -> true | _ -> false ) c
-    | Conj_pos c -> ( [Conj_pos c] , []  )
-    | Conj_neg c -> ( []  , [Conj_neg c] )
-    | Conj_nil   -> ( []  , []  )
-  )
-
-(* preconds -> preds 
-let partition_pred p = List.partition 
-      ( fun x -> match x with | Pred_pos _ -> true | _ -> false ) p
-*)
 (* ast.predicate -> ast.sym *)
 let predname_of_pred p =
   ( match p with
@@ -93,132 +142,166 @@ let intersect l1 l2 = List.filter (fun x -> List.mem x l2) l1
 let intersect_prednames l1 l2 = 
   intersect ( prednames_of_preds l1 ) ( prednames_of_preds l2 )
 
-(* 
+(* TODO: consolidate all these intersection methods
  * intersect_preds can be written more generally, but i only need 
  * it for a specific purpose. tail recursion would be cool too.
  *)
 (* ast.predicate list -> ast.predicate list -> ast.predicate list *)
-let rec intersect_preds l1 l2 =
-  ( match l1, l2 with 
-    | Pred_gnd( n1 , p1 )::t1 , Pred_gnd( n2 , p2 )::t2 when n1 = n2 ->
-      Pred_gnd( n1 , p1 )::intersect_preds t1 t2
-    | Pred_gnd( n1 , p1 )::t1 , Pred_var( n2 , p2 )::t2 when n1 = n2 ->
-      Pred_gnd( n1 , p1 )::intersect_preds t1 t2
-    | Pred_var( n1 , p1 )::t1 , Pred_gnd( n2 , p2 )::t2 when n1 = n2 ->
-      Pred_gnd( n2 , p2 )::intersect_preds t1 t2
-    | _  , []     -> []
-    | [] , _      -> []
-    | _::_ , _::_ -> []
-  )  
+(* p = reference pred name 
+ * l = list to match on 
+ *)
 
-(* 'a * 'b -> 'a *)
-let tup1 t = 
-( match t with 
-  | x , _ -> x
-)
-
-(* 'a * 'b -> 'b *)
-let tup2 t = 
-( match t with 
-  | _ , x -> x 
-)  
-
-(* incarnation of map2 with hash tables *)
-let rec map2_hashadd env names values = 
-( match ( names , values ) with
-  | ( [] , [] ) -> []
-  | n::t1 , v::t2 -> 
-    let e = Hashtbl.add env n v in e::map2_hashadd env t1 t2 
-  | ( _ , _ ) -> invalid_arg "add"
-) 
+(* ast.sym -> ast.predicate list -> ast.predicate list *)
+let intersect_preds p l =
+  let match_name = 
+    ( fun x -> if p = (predname_of_pred x) then true else false) in
+  List.filter match_name l
 
 (* incarnation of iter2 with hash tables *)
 let rec bind env names values = 
 ( match ( names , values ) with
   | ( [] , [] ) -> env
-  | n::t1 , v::t2 -> Atomhash.add env n v; bind env t1 t2 
+  | n::t1 , v::t2 -> Atomhash.replace env n v; bind env t1 t2 
   | ( _ , _ ) -> invalid_arg "bind"
 ) 
 
-(* create temporary bindings for comparison *)
-let extend sp pp = 
-  let env = Atomhash.create 2 and
-      names = predval_of_pred pp and
-      values = predval_of_pred sp in
-  bind env names values
-
-(* verifies if a set of temporary bindings is valid *)
-let bindings_valid env1 env2 = 
-  let names = Atomhash.fold (fun k _ acc -> k::acc) env1 [] in 
-  let rec loop acc names = 
-    ( match names with 
-      | [] -> []
-      | n :: t ->
-	let v = Atomhash.find env1 n 
-	and ref = Atomhash.find_all env2 n in
-	( match ref with
-	  | [] -> Atomhash.add env1 n v ; loop (true::acc) t
-	  | [r] -> if v = r then loop (true::acc) t else [false] 
-	  | r::_ -> failwith "duplicate bindings"
-	)
-    ) in List.fold_left (fun x y -> x || y ) true (loop [] names)
-
-(* state, action -> action list *)
-(* pred list -> action list -> action list *)
-(* ---------------------------------------*)
-(* applicable_actions 
- *
- * This function finds the applicable instances of an operator
- * for a given state. This is the meat of the planner.
- * 
- * Input 
- *         act: action to process
- *         env: a parameter substitution -- baby symbol table
- *           s: state to apply action
+(* 
+ * copy global env and update pp names with sp vals
  *)
-let rec applicable_actions acc act ( pp , np ) env s  =
-  ( match pp with
-    | [] -> 
-      (* TODO: add support for negative preds *)
-      let vals = Atomhash.fold (fun _ v acc -> v::acc) env [] in 
-      (Pred_gnd( act.name , vals )) :: acc (* op, grounded-param list *) 
-    | p :: pt -> (* choose a predicate *)
-      let sps = intersect_preds [p] s in
-      let rec loop sps = 
-	( match sps with 
-	  | [] -> failwith "empty intersection"
-	  | sp::t -> 
-	    let tenv = extend sp p in 
-	    if ( bindings_valid tenv env ) then(* tenv semantics wrong *)
-	      applicable_actions acc act ( pt , np ) tenv s
-	    else 
-	      loop t
-	) in loop sps
+let extend env sp pp = 
+  let test_env = Atomhash.copy env in
+  let names = predval_of_pred pp in
+  let values = predval_of_pred sp in
+  bind test_env names values
+
+let retract env pp =
+  let names = predval_of_pred pp in
+  List.iter (fun k -> Atomhash.remove env k ) names
+
+
+(* verifies if a set of temporary bindings is valid 
+ * 
+ * - get all the names (i.e. keys) of test bindings
+ * - loop over test names 
+ *   - find the corresponding test value
+ *   - get the reference values
+ *   - compare the test value with reference value;
+ *     add bindings when they don't exist;
+ *     
+ *)
+(* env1: test bindings
+ * env2: reference bindings
+ * this just checks if each is the same actually 
+ *)
+let dump_keys env =  Atomhash.fold (fun k _ acc -> k::acc) env []
+let dump_vals env =  Atomhash.fold (fun _ v acc -> v::acc) env []
+
+let dump_hash env = 
+  let keys = dump_keys env in
+  let vals = dump_vals env in
+  Printf.printf "\nKEYS:%s \nVALS:%s"
+  (string_of_syms(List.map string_of_atom keys)) 
+  (string_of_syms(List.map string_of_atom vals)) 
+
+let bindings_valid test_env ref_env = 
+  let test_names = dump_keys test_env in (*lookup every name in ref*)
+  let test_vals = dump_vals test_env in
+  let rec loop acc names values  = 
+    ( match ( names , values ) with 
+      | ( [] , [] ) -> acc
+      | (test_name::t1 , test_val::t2 )-> 
+	(try 
+	  let ref_val = Atomhash.find ref_env test_name in
+	  if test_val = ref_val then loop (true::acc) t1 t2
+	  else [false] (* value mismatch *)
+	with Not_found -> (* name not found *)
+(*
+	  let _ = Printf.printf "\nTEST NAME: %s = %s not found"
+	    (string_of_atom test_name) 
+	    (string_of_atom test_val) in
+	  let _ = Printf.printf "\nPRE REPLACEMENT" in
+	  let _ = Printf.printf "\n--TEST ENV--" in
+	  let _ = dump_hash test_env in
+	  let _ = Printf.printf "\n--REF ENV--" in
+	  let _ = dump_hash ref_env in
+*)
+	  let _ =  Atomhash.replace ref_env test_name test_val in
+(*	 
+	  let _ = Printf.printf "\nPOST REPLACEMENT" in
+	  let _ = Printf.printf "\n--TEST ENV--" in
+	  let _ = dump_hash test_env in
+	  let _ = Printf.printf "\n--REF ENV--" in
+	  let _ = dump_hash ref_env in
+*)	 
+	  loop acc t1 t2 )
+      | ( _ , _ ) -> 
+	let error_msg = "improper input" in
+	raise (Planner_error error_msg)
+    )
+      in List.fold_left (fun x y -> x && y ) true (loop [] test_names test_vals)
+    
+
+(* nice debug print
+let error_msg = 
+	"\nPOS PREDS:"^
+	(string_of_syms (List.map string_of_pred pp)) ^
+	"\nP:"^
+	(string_of_syms (List.map string_of_pred [p])) ^
+	"\nSTATE: " ^ 
+	(string_of_syms (List.map string_of_pred s)) ^
+	"\nINTERSECTION:" ^
+	(string_of_syms (List.map string_of_pred sps))
+      in
+      failwith error_msg;
+*)
+
+(* DON'T DELETE UNTIL DOCUMENTED
+   action parameters will appear in the order they're declared
+   in the parameters section
+*)
+let name_of_param atom =
+  ( match atom with
+    | Atom_gnd a ->
+      let stop = (String.index a '-') in
+      let name =  String.sub a 0 stop in
+      Atom_gnd name
+    | Atom_var a ->
+      let stop = (String.index a '-') in
+      let name =  String.sub a 0 stop in
+      Atom_var name
+    | _ -> 
+      let error_msg = "usage" in
+      failwith error_msg
   )
 
-(* fetch action list from symbol table *)
-let actions_of_env env = failwith "not implemented"
+let ground_action env act =
+  let names = List.map name_of_param act.parameters in
+  let vals = List.map (fun n -> Atomhash.find env n) names in
+  Pred_gnd( act.name , vals ) 
 
 (* 'a list -> 'a list -> bool *)
 let goal_test s1 s2 = 
-  let s12 = intersect s1 s2 in
-  ( match s12 with
+  let intersection = intersect s1 s2 in
+  ( match intersection with
     | [] -> true
     | _ -> false
   ) 
 
-(* loop over all operators and accumulate applicable ops 
-let app_ops s ops = 
-  let rec loop acc ops =
-    ( match ops with 
-      | [] -> acc 
-      | op::t -> 
-	let ( pp , np ) = partition_preds op.precondition
-	and env = Atomhash.create 10
-	and ao = applicable_actions [] op ( pp , np ) env s
-	in loop ( ao::acc ) t
-    ) in loop [] ops
-*)
+(* preconds -> pred *)
+let partition_conjunction conj = 
+  ( match conj with 
+    | Conj_and c -> List.partition 
+      ( fun x -> match x with | Conj_pos _ -> true | _ -> false ) c
+    | Conj_pos c -> ( [Conj_pos c] , []  )
+    | Conj_neg c -> ( []  , [Conj_neg c] )
+    | Conj_nil   -> ( []  , []  )
+  )
+
+let partition_to_predicates conj = 
+  let ( pc , nc ) = partition_conjunction conj in
+  let pred_of = List.map pred_of_conj in
+  ( pred_of pc , pred_of nc )
+
 (* 'a -> 'a list -> 'a list *)
 let remove_pred l p = 
   let rec loop acc l =
@@ -230,51 +313,210 @@ let remove_pred l p =
     )
   in loop [] l
 
+let find_action acts name =
+  let filter_name = 
+    (fun a -> if a.name = name then true else false ) in 
+  List.find filter_name acts
+
+(* returns an enviornment mapping parameter names to values *)
+let get_action_bindings act act_def =
+  let size = List.length act_def.parameters in
+  let env = Atomhash.create size in
+  let names = List.map name_of_param act_def.parameters in
+  let vals = predval_of_pred act in
+  let _ = List.iter2 ( fun n v -> Atomhash.add env n v ) names vals in
+  env
+
+let ground_pred env pred =
+  let name = predname_of_pred pred in
+  let param_names = predval_of_pred pred in (* var vals = param names *)
+  let vals = 
+    List.map ( fun n -> Atomhash.find env n ) param_names in
+  ( match pred with 
+    | Pred_var( name , _ ) -> Pred_gnd( name , vals )
+    | _ -> 
+      let error_msg = "Failed to ground predicate" in
+      raise ( Planner_error error_msg )
+  )
+
+(* partitions an action effect and outputs the bound predicates *)
+let partition_to_grounded_effect acts act =
+  let name = predname_of_pred act in
+  let act_def = find_action acts name in
+  let env = get_action_bindings act act_def in (* create bindings *)
+  let ( pp , np ) = partition_to_predicates act_def.effect in
+  let pe = List.map (ground_pred env) pp in (* list of preds*)
+  let ne = List.map (ground_pred env) np in
+  ( pe , ne )
+
+let complimentary_effects act act_defs = 
+  let ( pe , ne ) = partition_to_grounded_effect act_defs act in
+  let intersection = intersect pe ne in
+  ( match intersection with 
+    | [] -> false
+    | _ -> true
+  )
+
+let  remove_complimentary_effects acts act_defs =
+  let rec loop acc a =
+  ( match a with
+    | [] -> List.rev acc
+    | h::t -> 
+      if (complimentary_effects h act_defs) then loop acc t 
+      else loop (h::acc) t
+  )
+  in loop [] acts
+
+
+let rec applicable_actions acc act act_defs ( pos_preds , neg_preds ) env s  =
+  ( match pos_preds with
+    | [] ->       (* TODO: add support for negative preds *)
+      let a = ground_action env act in
+      let cmp_eff = true&&(complimentary_effects a act_defs) in 
+      if cmp_eff then 
+      else a::acc
+    | pp :: pt -> (* choose a predicate *)
+      let name = predname_of_pred pp in
+      let sps = intersect_preds name s in (* common ppreds with state *)
+     (* let error_msg = 
+	    "\nNAME: " ^
+	    (name) ^
+	    "\nSTATE: " ^
+	    (string_of_syms (List.map string_of_pred s)) ^
+	    "\nINTERSECTION: " ^
+	    (string_of_syms (List.map string_of_pred sps))
+      in
+      failwith error_msg;*)
+      let rec loop sps = 
+	( match sps with 
+	  | [] -> 
+	    let error_msg = "state has no matching preds with " ^ 
+	    "\nNAME: " ^
+	    (name) ^
+	    "\nSTATE: " ^
+	    (string_of_syms (List.map string_of_pred s)) ^
+	    "\nSP: " ^
+	    (string_of_syms (List.map string_of_pred sps))
+	    in
+	    raise (Planner_error error_msg)
+	  | sp::t -> 
+(*	    let _ =
+
+	      Printf.printf"\n\nSPS:%s" 
+	      (string_of_syms (List.map string_of_pred sps)) in
+*)
+	    let tenv = extend env sp pp in
+(*
+	    let _ = Printf.printf "\nTEST ENV" in
+	    let _ = dump_hash tenv in
+	    let _ = Printf.printf "\nREF ENV" in
+	    let _ = dump_hash env in
+*)	    
+	    let valid_bindings = bindings_valid tenv env in
+	   
+	    if ( valid_bindings ) then (
+	      
+	      if ( pt = [] ) then
+	      
+	      
+	      applicable_actions acc act act_defs ( pt , neg_preds ) tenv s
+	    ) 
+	    else (
+	      let _ = retract env pp in
+	      loop t )
+	) in loop sps
+  )
+
+(* loop over all operators and accumulate applicable ops *)
+(* ast.pred list -> ast.action list -> ast.pred list *)  
+let app_ops s acts = 
+  let rec loop acc ops =
+    ( match ops with 
+      | [] -> List.flatten acc 
+      | op::t -> 
+	let ( pp , np ) = partition_to_predicates op.precondition in
+	let env = Atomhash.create 10 in
+	let ao = applicable_actions [] op acts ( pp , np ) env s in
+
+	let _ = Printf.printf "\nAPP ACTIONS: %s"
+	  (string_of_syms (List.map string_of_pred ao))
+	in
+
+	let ao = remove_complimentary_effects ao acts in
+	
+	let _ = Printf.printf "\nAPP ACTIONS: %s"
+	  (string_of_syms (List.map string_of_pred ao))
+	in
+	
+	loop ( ao::acc ) t
+    ) in loop [] acts
+  
 (* TODO: successor function *)
 (* state, op -> state *)
-(* ast.predicate list -> ast.predicate -> ast.predicate list 
-let succ s o env = (* o is a grounded pred list *)
-  let act = Atomhash.find env (predname_of_pred o) (* TODO: globalhash *)
-  and ( pos , neg ) = partition_preds act 
-  and ne = List.map pred_of_conj neg    (* pred list *)
-  and pe = List.map pred_of_conj pos in (* pred list *)
-  let rec loop s ne =
+(* ast.predicate list -> ast.predicate -> ast.predicate list *)
+(*
+ * - remove negative effects
+ * - apply positive effects
+ *)
+let succ state act acts = (* act is a grounded pred list *)
+  let ( pe , ne ) = partition_to_grounded_effect acts act in
+  
+  let _ = Printf.printf "\nPOS EFFECTS: %s"
+    (string_of_syms (List.map string_of_pred pe)) in
+  let _ = Printf.printf "\nNEG EFFECTS: %s"
+    (string_of_syms (List.map string_of_pred ne)) in
+
+  let rec loop s ne = (* loop through negative effects first *)
     ( match ne with
       | [] -> 
-	let rec iloop s pe = 
+	let rec iloop s pe =  (* loop through positive *)
 	  ( match pe with
-	    | [] -> s
+	    | [] -> List.rev s
 	    | h::t -> iloop (h::s) t
 	  )
 	in iloop s pe
       | h::t -> loop (remove_pred s h) t 
     )
-  in loop s ne
-*)
+  in loop state ne
+
 (* TODO: planner!!!! 
  * - how to handle cycling 
  * - how to handle deadends -- backtrack to op choice
  * - how to handle multiple goal states
  * - how to handle complimentary effects 
- *
-let fsearch problem = 
-  let ops = problem.actions 
-  and s0 = problem.init 
-  and g = problem.goal in 
-  let rec loop plan s ops =
-    if ( goal_test s g ) then plan
-    else 
-      let app = app_ops s ops in (* set of all app ops in s *)
-      ( match app with
-	| [] -> failwith "no applicable operators"
-	| o::t ->             (* choose applicable operator *)
-	  let s = succ s o in (* TODO: transistion to next state *)
-	  loop (o::plan) s t  (* plan from there *)
-      )
-  in loop [] s0
+ * 
+ * - a cool spot to parallelize is the applicable action choice
+ *  - use consistent notation with this and report
  *)
-let plan problem =
-  failwith "progress"
+let fsearch problem = 
+  let { init = s0 ; goal = g ; ops = acts } = problem in
+  let rec loop plan s aset =
+    if ( goal_test s g ) then plan
+    else
+      let app = app_ops s aset in (* set of all app ops in s *)
+      
+      let _ = Printf.printf "\nSTATE = %s "
+	(string_of_syms (List.map string_of_pred s)) in
+(*
+      let _ = Printf.printf "\nAPP OPS = %s "
+	(string_of_syms (List.map string_of_pred app)) in
+*)
+      ( match app with (* app = list of preds *)
+	| [] -> 
+	  let error_msg = "no applicable operators" in
+	  raise (Planner_error error_msg)
+	| a::t -> (* TODO: implement backtracking!*)
+          let s = succ s a acts in
+	  
+	  let _ = Printf.printf "\nSUCC: %s"
+	    (string_of_syms (List.map string_of_pred app)) in
+	  loop (a::plan) s acts  (* plan from there *)
+      )
+  in loop [] s0 acts
+
+let solve problem =
+  let plan = fsearch problem in
+  failwith "PROGRESS!!!"
 
 let planner_test infile =
   let lexbuf = Lexing.from_channel infile in
@@ -282,9 +524,9 @@ let planner_test infile =
   let rec loop env =
     let sexpr  = Parser.parse Lexer.token lexbuf in
     match sexpr with
-      | None -> 
-	(* plan here *)
-	() 
+      | None ->
+	let problem = env.problem in 
+	let plan = solve problem in () 
       | Some s ->
         let ast = ast_of_sexpr s in
 	let _ = strips_of_ast env ast in
