b7e83cb96a94ed6b2a048cdb7c7b491a818e7ccd
John Martin <jdmartin86@gmail.com>
2014-05-08 08:52:55 -0400

    Initial commit of runtime environment

---
+++ code/src/execute.ml
@@ -0,0 +1,23 @@
+open Ast
+open Strips 
+
+
+let rec execute_instructions instructions =
+  let stack = Array.make 1024 "" in
+  let rec exec fp sp pc =
+    ( match instructions.(pc) with 
+      | Push_action action ->
+	stack.(sp) <- action ;
+	exec fp (sp + 1) (pc + 1)
+      | Push_pred predicate ->
+	stack.(sp) <- predicate ;
+	exec fp (sp + 1) (pc + 1)
+      | Pop_pred -> 
+	exec fp (sp - 1) (pc + 1)
+      | Goal -> ()    
+    )
+  in exec 0 0 0
+
+let execute_bytecode instructions = 
+  execute_instructions (Array.of_list instructions) 
+ 
